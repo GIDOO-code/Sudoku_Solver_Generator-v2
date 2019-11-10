62043cc95a6d610994b112064778bb8f16f6269f
GIDOO-config <katsuroj01@gmail.com>
2019-11-10 16:20:14 +0900

    update

--- docs/js/naviHeader.js
+++ docs/js/naviHeader.js
@@ -7,7 +7,7 @@ function writeNaviHeader(){
 	html += '<li><a href="page2.html">Sudoku Algorithm</a></li>';
 	html += '<li><a href="page1.html">Element Tech</a></li>';
 	html += '<li><a href="page17.html">Download</a></li>';
-	html += '<li><a href="page18.html">Comment</a></li>';
+//	html += '<li><a href="page18.html">Comment</a></li>';
     html += '<li><a href="page19.html">about this</a></li>';
 //    html += '<li><a href="http://csdenp.web.fc2.com/index.html">To Japanese HP</a></li>';
 	
